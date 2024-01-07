b0f9e1779c4d6dec88c72d08823cab95c0c352db
Jakir Hasan <52996649+Jak57@users.noreply.github.com>
2024-01-07 13:09:17 +0600

    Create p1. Subsets.py

---
+++ 10. Subsets/p1. Subsets.py
@@ -0,0 +1,11 @@
+https://leetcode.com/problems/subsets/
+class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        subsets = [[]]
+        for num in nums:
+            subsets = (subsets + [item + [num] for item in subsets])
+        return subsets
